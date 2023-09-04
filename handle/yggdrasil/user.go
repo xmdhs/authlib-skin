bfb9a157d5473521669b4854a9e24e1ae9d1543d
xmdhs <xmdhss@gmail.com>
2023-09-04 22:33:02 +0800

    移动代码

--- handle/yggdrasil/user.go
+++ handle/yggdrasil/user.go
@@ -1,14 +1,41 @@
 package yggdrasil
 
 import (
+	"encoding/json"
 	"errors"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
 	"github.com/xmdhs/authlib-skin/model/yggdrasil"
 	sutils "github.com/xmdhs/authlib-skin/service/utils"
+	yggdrasilS "github.com/xmdhs/authlib-skin/service/yggdrasil"
 	"github.com/xmdhs/authlib-skin/utils"
 )
+
+func (y *Yggdrasil) Authenticate() httprouter.Handle {
+	return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		cxt := r.Context()
+		a, err := utils.DeCodeBody[yggdrasil.Authenticate](r.Body, y.validate)
+		if err != nil {
+			y.logger.DebugContext(cxt, err.Error())
+			handleYgError(cxt, w, yggdrasil.Error{ErrorMessage: err.Error()}, 400)
+			return
+		}
+		t, err := y.yggdrasilService.Authenticate(cxt, a)
+		if err != nil {
+			if errors.Is(err, yggdrasilS.ErrPassWord) || errors.Is(err, yggdrasilS.ErrRate) {
+				y.logger.DebugContext(cxt, err.Error())
+				handleYgError(cxt, w, yggdrasil.Error{ErrorMessage: "Invalid credentials. Invalid username or password.", Error: "ForbiddenOperationException"}, 403)
+				return
+			}
+			y.logger.WarnContext(cxt, err.Error())
+			handleYgError(cxt, w, yggdrasil.Error{ErrorMessage: err.Error()}, 500)
+			return
+		}
+		b, _ := json.Marshal(t)
+		w.Write(b)
+	}
+}
 
 func (y *Yggdrasil) Validate() httprouter.Handle {
 	return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
