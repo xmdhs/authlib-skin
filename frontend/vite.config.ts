48aabb74f4b8deb6a0492a2fc6666c12613a7b14
xmdhs <xmdhss@gmail.com>
2023-10-11 22:15:22 +0800

    兼容旧浏览器

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { join } from "path";
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    legacy()
+  ],
   resolve: {
     alias: {
       '@': join(__dirname, "src")
