b0bd44eebb9182bb151076f398c6d1a21b415df3
xmdhs <xmdhss@gmail.com>
2023-10-12 20:46:07 +0800

    为鉴权中间件添加测试

---
+++ test/auth_test.go
@@ -0,0 +1,35 @@
+package test
+
+import (
+	"encoding/json"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/xmdhs/authlib-skin/model"
+)
+
+func TestAuthMiddleware(t *testing.T) {
+	t.Parallel()
+
+	rep, err := http.Get("http://127.0.0.1:8080/api/v1/user")
+	require.Nil(t, err)
+	defer rep.Body.Close()
+
+	require.Equal(t, rep.StatusCode, 401)
+
+	reqs, err := http.NewRequest("GET", "http://127.0.0.1:8080/api/v1/user", nil)
+	require.Nil(t, err)
+	reqs.Header.Add("Authorization", "Bearer aaaaaaaaaaa")
+	rep1, err := http.DefaultClient.Do(reqs)
+	require.Nil(t, err)
+	defer rep1.Body.Close()
+
+	assert.Equal(t, rep.StatusCode, 401)
+
+	var api model.API[any]
+	require.Nil(t, json.NewDecoder(rep1.Body).Decode(&api))
+
+	assert.Equal(t, api.Code, 5)
+}
---
+++ test/server_test.go
@@ -0,0 +1,28 @@
+package test
+
+import (
+	"context"
+	"os"
+	"testing"
+
+	"github.com/pelletier/go-toml/v2"
+	"github.com/samber/lo"
+	"github.com/xmdhs/authlib-skin/config"
+	"github.com/xmdhs/authlib-skin/server"
+	"go.uber.org/goleak"
+)
+
+func TestMain(m *testing.M) {
+	ctx := context.Background()
+	b := lo.Must(os.ReadFile("config.toml"))
+	var config config.Config
+	lo.Must0(toml.Unmarshal(b, &config))
+	s, cancel := lo.Must2(server.InitializeRoute(ctx, config))
+
+	go func() {
+		s.ListenAndServe()
+	}()
+
+	goleak.VerifyTestMain(m)
+	cancel()
+}
