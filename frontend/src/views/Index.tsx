330bbfa50316698147e03fa1b1e08c7e3dbe80a6
xmdhs <xmdhss@gmail.com>
2023-10-13 22:21:09 +0800

    fix

--- frontend/src/views/Index.tsx
+++ frontend/src/views/Index.tsx
@@ -2,7 +2,7 @@ import Profile from "@/views/profile/Profile"
 import Login from "@/views/Login"
 
 export default function Index() {
-    if (!(localStorage.getItem("token") || localStorage.getItem("token") == '""')) {
+    if (localStorage.getItem("token") && localStorage.getItem("token") != '""') {
         return <Profile />
     }
     return <Login />
